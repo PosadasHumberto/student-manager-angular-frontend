0e4e8d352f98e0f1c7fd1e0ef99d8fc6f38a7633
Humberto Posadas <posadas.humberto@outlook.com>
2023-03-13 19:01:29 +0100

    Adding modal with form & add Student method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Student} from "./models/student";
 import {StudentService} from "./services/student.service";
 import {HttpErrorResponse} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -13,10 +15,12 @@ export class AppComponent implements OnInit{
   //attributes
   public title = 'Students Manager';
   public students : Student[];
+  public editStudent : Student;
+  public deleteStudent : Student;
 
   //constructor
   constructor(
-    private studentService : StudentService
+    private studentService : StudentService,
   ) {
 
   }
@@ -32,5 +36,58 @@ export class AppComponent implements OnInit{
       (response : Student[]) => {this.students = response;},
       (error : HttpErrorResponse) => {alert(error.message)}
     )
+  }
+
+  /*Método que va a crear un boton en el DOM el cual se va a encargar de lanzar
+  * la ventana modal que corresponda a la accion deseada (crear, modificar o
+  * eliminar un estudiante*/
+  public onOpenModal(student : Student | null, mode : String) {
+
+    //recuperando el elemento que va a mostrar el boton
+    const container = document.getElementById("main-container")
+
+    //creando el boton programaticamente
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    //establecr el target del boton segun en donde ha sido presionado lanzara
+    // el modal que corresponde
+    if (mode === "add"){
+      button.setAttribute('data-target', '#addStudentModal')
+    }
+
+    if (mode === "edit"){
+      button.setAttribute('data-target', '#editStudentModal')
+    }
+
+    if (mode === "delete"){
+      button.setAttribute('data-target', '#deleteStudentModal')
+    }
+
+    //agregar el boton al elemento container
+    container?.appendChild(button);
+    button.click();     //The click() method simulates a mouse-click on an element
+  }
+
+  onAddStudent(addForm : NgForm) :void {
+
+    this.studentService.addStudent(addForm.value as Student).subscribe(
+      (response : Student) => {
+        console.log(response);
+        this.getStudents(); //relanzar la carga de los estudiantes en el arreglo Sudents[] que sera mostrado a la vista
+        addForm.resetForm();
+        document.getElementById('add-student-form')?.click();
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+
+  }
+
+  onUpdateStudent(editForm : NgForm){
+
   }
 }
